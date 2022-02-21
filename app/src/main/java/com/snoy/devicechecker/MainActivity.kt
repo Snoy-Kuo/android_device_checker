8c1972e3e5c7a4d194aca5ee610117b1835db166
Snoy Kuo <snoy.kuo@twprotech.com>
2022-02-21 15:06:54 +0800

    add SSAID info.

--- app/src/main/java/com/snoy/devicechecker/MainActivity.kt
+++ app/src/main/java/com/snoy/devicechecker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.snoy.devicechecker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
@@ -12,6 +14,9 @@ class MainActivity : AppCompatActivity() {
     private val tvRandomUUID: TextView by lazy { findViewById(R.id.tvRandomUUID) }
     private val btnGenRandomUUID: Button by lazy { findViewById(R.id.btnGenRandomUUID) }
 
+    private val tvSSAID: TextView by lazy { findViewById(R.id.tvSSAID) }
+    private val btnGenSSAID: Button by lazy { findViewById(R.id.btnGenSSAID) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +25,24 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         initUUID()
+        initSSAID()
+    }
+
+    private fun initSSAID() {
+        btnGenSSAID.setOnClickListener { showSSAID() }
+        showSSAID()
+    }
+
+    private fun showSSAID() {
+        val ssaid = getSSAID()
+        tvSSAID.text = ssaid
+    }
+
+    @SuppressLint("HardwareIds")
+    private fun getSSAID(): String {
+        val ssaid = Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID)
+        Log.d("RDTest", "SSAID = $ssaid")
+        return ssaid
     }
 
     private fun initUUID() {
